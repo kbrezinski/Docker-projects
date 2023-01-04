683c3bd73471bcbc151fd80b1e4875c928965cab
Kenneth Brezinski <kenbrezinski@live.com>
2023-01-03 22:05:25 -0600

    Add precompiled assets for Heroku

--- pages/1_Stock_Price_App_📈.py
+++ pages/1_Stock_Price_App_📈.py
@@ -3,28 +3,47 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
-# user input
 st.title('Stock Price App :chart_with_upwards_trend:')
-num_days = st.number_input("Enter Number of Days to Track", min_value=1, max_value=360*5, value=14, step=7)
-today_date = datetime.today()
 
+@st.cache()
+def get_today_date():
+    return datetime.today()
+today_date = get_today_date()
+
+# User Input Columns
+#checkboxes, ranges = st.columns(2)
+
+start_time, end_time = st.slider(
+        "Enter Date Range for your Portfolios",
+        min_value=today_date - timedelta(days=365*5),
+        value=(today_date - timedelta(days=365), today_date),
+        step=timedelta(days=30),
+        format="MM/DD/YY")
+# Print the selected dates
+st.write(f"Range Date: {start_time.strftime('%Y-%m-%d')} - {end_time.strftime('%Y-%m-%d')}")
+
+# Added a checkbox to show year to date performance
+ytd = st.checkbox('Year To Date (Overrides the Slider)', value=False)
+if ytd: start_time = datetime(today_date.year, 1, 1, 1, 1)
+
+# Portfolio Columns
 Portfolio1, Portfolio2, Portfolio3 = st.columns(3)
-defaults = ['AAPL', 'TSLA', '']
+defaults = ['SPY', 'EWC', 'TSLA META AMZN GOOGL MSFT']
 series = {}
 
-
-for i, col in enumerate([Portfolio1, Portfolio2, Portfolio3]):
+for i, (col, header) in enumerate(
+                zip([Portfolio1, Portfolio2, Portfolio3], [f'Portfolio 1', f'Portfolio 2', "Dad's Picks"])):
     with col:
-        col.subheader(f'Portfolio {i+1}')
+        col.subheader(header)
 
         symbols = st.text_input('Enter Stock Ticker Symbols', defaults[i],
                     placeholder="MSFT GOOGL", key=f"symbol{i}")
 
         # download data
         if symbols != '':
-            data = yf.download(symbols, today_date - timedelta(days=num_days), today_date, group_by='column', threads=2)
+            data = yf.download(symbols, start_time, end_time, group_by='column', threads=2)
             
             if data.empty:
                 st.error(f'Invalid stock ticker {symbols}', icon="🚨")
